46de8c997270cd4e352a61c8c823dcaf0c1bdcae
singaidance <singaidance@gmail.com>
2016-09-25 18:51:29 +0800

    Created by Coding Ground Client

---
+++ javaHelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+
+        int a = 10;
+        int b = 2;
+
+        System.out.println("a = " + a +  ", b = " + b +  ", a*b = " + (a*b));
+     }
+}
