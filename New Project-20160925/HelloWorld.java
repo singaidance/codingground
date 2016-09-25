6f7e20b8fbe76a8230a59d4f107deda4de08da9a
singaidance <singaidance@gmail.com>
2016-09-25 18:44:55 +0800

    Created by Coding Ground Client

---
+++ New Project-20160925/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        String wifesName = "Ssss";
+        System.out.println("Hello Mr Saravanan and " + wifesName);
+        int a = 10;
+        int b = 2;
+
+        System.out.println("a = " + a +  ", b = " + b +  ", a*b = " + (a*b));
+     }
+}
